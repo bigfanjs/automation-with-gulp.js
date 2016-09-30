9f7044ddc30a30fa2baceb95c137a2adfbb7ebc2
AdelJS <bigfanjs@gmail.com>
2016-10-01 00:18:32 +0100

    updating tasks/test-browser [directly assigning karma config file]

--- tasks/test-browser.js
+++ tasks/test-browser.js
@@ -1,8 +1,10 @@
 import karma from 'karma';
 
 export const Server = karma.Server;
-export default function ( configFile ) {
+export default function () {
   return callback => {
-    (new Server({ configFile }, callback)).start();
+    (new Server({
+      configFile: 'karma.conf.js'
+    }, callback)).start();
   };
 }
